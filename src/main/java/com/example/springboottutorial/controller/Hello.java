b320fd28a8fdc8a79efec9c79f609bd23f83a079
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-06-07 11:22:33 +0530

    Added: Created Hello World Controller

--- src/main/java/com/example/springboottutorial/controller/Hello.java
+++ src/main/java/com/example/springboottutorial/controller/Hello.java
@@ -1,12 +1,29 @@
 package com.example.springboottutorial.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Hello {
     @GetMapping(value = {"","/","/Tanuja"})
     public String helloWorld(){return "Hello From BridgeLabz";}
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello "+ name + "!";
+    }
+
+    @GetMapping(value = {"","/","/Get"})
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello "+name+"!";
+    }
+
+    @PostMapping("/post-name")
+    public String sayHello(@RequestBody User user) {
+        User user1 =new User();
+        return "Hello "+user1.getFirstName()+" "+user1.getLastName();
+    }
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+        return "Hello "+firstName+" "+lastName+"!";
+    }
 }
